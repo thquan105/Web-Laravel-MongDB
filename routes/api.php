c1b52fb46416b28b60c54049781a912087ca995b
Thquan105 <thquan105@gmail.com>
2023-12-05 19:52:57 +0700

    Create project + link MongoDB

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get('/ping', function (Request $request) {
+    $connection = DB::connection('mongodb');
+    $msg = 'MongoDB is accessible!';
+
+    try {
+        $connection->command(['ping' => 1]);
+    } catch (\Exception $e) {
+        $msg = 'MongoDB is not accessible. Error: ' . $e->getMessage();
+    }
+
+    return ['msg' => $msg];
+});
